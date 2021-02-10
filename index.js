3b3e1d822a0b41057a2a6da1a91fd3819d4e2ff9
wishzhang <1535703141@qq.com>
2021-02-11 01:23:00 +0800

    解决ip问题

--- index.js
+++ index.js
@@ -26,12 +26,14 @@ const server = createSocketServer(app);
 
 function getIPAddress() {
   var interfaces = require('os').networkInterfaces();
-  for (var devName in interfaces) {
-    var iface = interfaces[devName];
-    for (var i = 0; i < iface.length; i++) {
-      var alias = iface[i];
-      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
-        return alias.address;
+  for (let [networkType, networks] of Object.entries(interfaces)) {
+    if (networkType.startsWith('VM')) continue;
+
+    for (let network of networks) {
+      if (network.family === 'IPv4'
+        && network.address !== '127.0.0.1'
+        && !network.internal) {
+        return network.address;
       }
     }
   }
