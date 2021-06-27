dd1df439cd695296844b0ea78b9657010620018f
wishzhang <1535703141@qq.com>
2021-06-27 11:16:41 +0800

    修改 跨域

--- wish-xinmi-nodets-server/src/server.ts
+++ wish-xinmi-nodets-server/src/server.ts
@@ -1,6 +1,6 @@
 import errorHandlerMiddleware from "./middleware/error-handler-middleware";
 import Koa from "koa";
-import cors from 'koa2-cors';
+import cors from '@koa/cors';
 import socket from "./socket/index";
 import {setupDatabase} from "./dao/model";
 import debug from './util/debug';
@@ -16,10 +16,21 @@ const log = debug('server');
 
 const app = new Koa();
 
-setupDatabase();
-
 // 跨域
-app.use(cors());
+app.use(
+    cors({
+        origin: function (ctx) { //设置允许来自指定域名请求
+            return '*';
+        },
+        maxAge: 5, //指定本次预检请求的有效期，单位为秒。
+        credentials: true, //是否允许发送Cookie
+        allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], //设置所允许的HTTP请求方法
+        allowHeaders: ['Content-Type', 'Authorization', 'Accept', 'xinmi-token'], //设置服务器支持的所有头信息字段
+        exposeHeaders: ['WWW-Authenticate', 'Server-Authorization', 'xinmi-token'] //设置获取其他自定义字段
+    })
+);
+
+setupDatabase();
 
 app.use(beforeMiddleware());
 
